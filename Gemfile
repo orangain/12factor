d99a9489bfe0e18bc6d42fcf4f26192ea7fac231
orangain <orangain@gmail.com>
2016-01-13 19:34:40 +0900

    Permanently redirect all the requests to http://12factor.net/ja/

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source 'http://rubygems.org'
 
 gem 'sinatra', '1.2.6'
 gem 'thin', '1.2.7'
-gem 'maruku', :git => 'https://github.com/bhollis/maruku.git'
--- web.rb
+++ web.rb
@@ -1,39 +1,5 @@
 require 'sinatra'
-require 'maruku'
 
-get '/' do
-  erb :home
-end
-
-TOC = %w(codebase dependencies config backing-services build-release-run processes port-binding concurrency disposability dev-prod-parity logs admin-processes)
-
-get '/:factor' do |factor|
-  halt 404 unless TOC.include?(factor)
-  @factor = factor
-  erb :factor
-end
-
-helpers do
-  def render_markdown(file)
-    markdown = File.read("content/#{file}.md")
-    Maruku.new(markdown).to_html
-  rescue Errno::ENOENT
-    puts "No content for #{file}, skipping"
-  end
-
-  def render_prev(factor)
-    idx = TOC.index(factor)
-    return if idx == 0
-    "<a href=\"/#{TOC[idx-1]}\">&laquo; Previous</a>"
-  end
-
-  def render_next(factor)
-    idx = TOC.index(factor)
-    return if idx == TOC.size-1
-    "<a href=\"/#{TOC[idx+1]}\">Next &raquo;</a>"
-  end
-end
-
-not_found do
-  "Page not found"
+get '/*' do
+  redirect 'http://12factor.net/ja/' + params['splat'].first, 301
 end
